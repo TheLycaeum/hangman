2fc059a73e6e3971069e0bc69c538d445acde406
Noufal Ibrahim <noufal@nibrahim.net.in>
2017-09-30 15:37:12 +0530

    RED : Adds test for invalid words

--- test_hangman.py
+++ test_hangman.py
@@ -1,5 +1,7 @@
 import os
 
+import pytest
+
 import hangman
 
 def test_choose_word():
@@ -13,5 +15,18 @@ def test_choose_word():
     os.unlink("/tmp/fake_dict.txt")
 
     assert selected_word in ["elephant", "chimpanzee"]
+
+def test_choose_word_no_valid_words():
+    # First create a dummy file
+    f = open("/tmp/fake_dict.txt", "w")
+    for i in ["cat", "dog", "bull", "mouse", "personage's"]:
+        f.write(i+"\n")
+    f.close()
+
+    with pytest.raises(ValueError):
+        selected_word = hangman.choose_word("/tmp/fake_dict.txt")
+
+    os.unlink("/tmp/fake_dict.txt")
+
     
     
