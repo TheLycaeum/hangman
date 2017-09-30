54f7e2e08158daf2aa50c996b6371382085a5928
Noufal Ibrahim <noufal@nibrahim.net.in>
2017-09-30 15:36:56 +0530

    Green : Implements choose word

---
+++ hangman.py
@@ -0,0 +1,16 @@
+import random
+
+def choose_word(fname):
+    good_words = []
+
+    f = open(fname)
+    for i in f:
+        i = i.strip()
+        if len(i) >= 6:
+            good_words.append(i)
+    f.close()
+    
+    return random.sample(good_words, 1)[0]
+
+    
+    
